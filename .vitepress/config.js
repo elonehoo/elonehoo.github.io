e048f2cdbd2516d7c289a56c62acb56579b4a589
xiaoxunyao <huchengyea@163.com>
2022-02-22 20:24:08 +0800

    config: update config

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 export default defineConfig({
   base:'/',
-  title: 'The Elone Hoo Point',
+  title: 'The Elone Hoo Blog',
   description: "show Elhone Hoo's blog",
   head: [
     [
@@ -10,7 +10,7 @@ export default defineConfig({
       {
         rel: 'icon',
         type: 'image/x-icon',
-        href: '/favicon.ico'
+        href: '/logo.svg'
       }
     ]
   ],
