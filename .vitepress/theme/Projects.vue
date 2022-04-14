383c4f4d51407bac50ef18a30a7bd0266af22190
xiaoxunyao <huchengyea@163.com>
2022-04-14 21:34:29 +0800

    fix: add `games`

--- .vitepress/theme/Projects.vue
+++ .vitepress/theme/Projects.vue
@@ -22,32 +22,56 @@
 
     <div>
       <template v-for="key in Object.keys($frontmatter.projects)" :key="key">
-    <h4 class="mt-10 font-bold">
-      {{ key }}
-    </h4>
-    <div class="project-grid py-2 -mx-3 gap-2">
-      <a
-        v-for="item, idx in $frontmatter.projects[key]"
-        :key="idx"
-        class="item relative flex items-center"
-        :href="item.link"
-        target="_blank"
-        :class="!item.link ? 'opacity-0 pointer-events-none h-0 -mt-8 -mb-4' : ''"
-      >
-        <div v-if="item.icon" class="pt-2 pr-5">
-          <Proskit v-if="item.icon === 'proskit'" class="text-4xl opacity-50" />
-          <Venmo v-else-if="item.icon === 'venmoAdmin'" class="text-4xl opacity-50" />
-          <Highlight v-else-if="item.icon === 'paragraphHighlight'" class="text-4xl opacity-50" />
-          <Typing v-else-if="item.icon === 'typing'" class="text-4xl opacity-50" />
-          <Unknown v-else class="text-4xl opacity-50" />
+        <h4 class="mt-10 font-bold">
+          {{ key }}
+        </h4>
+        <div class="project-grid py-2 -mx-3 gap-2">
+          <a
+            v-for="item, idx in $frontmatter.projects[key]"
+            :key="idx"
+            class="item relative flex items-center"
+            :href="item.link"
+            target="_blank"
+            :class="!item.link ? 'opacity-0 pointer-events-none h-0 -mt-8 -mb-4' : ''"
+          >
+            <div v-if="item.icon" class="pt-2 pr-5">
+              <Proskit v-if="item.icon === 'proskit'" class="text-4xl opacity-50" />
+              <Venmo v-else-if="item.icon === 'venmoAdmin'" class="text-4xl opacity-50" />
+              <Highlight v-else-if="item.icon === 'paragraphHighlight'" class="text-4xl opacity-50" />
+              <Typing v-else-if="item.icon === 'typing'" class="text-4xl opacity-50" />
+              <Jumper v-else-if="item.icon === 'jumper'" class="text-4xl opacity-50" />
+              <TZFE v-else-if="item.icon === '2048'" class="text-4xl opacity-50" />
+              <Sudoku v-else-if="item.icon === 'sudoku'" class="text-4xl opacity-50" />
+              <Mathler v-else-if="item.icon === 'mathler'" class="text-4xl opacity-50" />
+              <Dwen v-else-if="item.icon === 'dwen'" class="text-4xl opacity-50" />
+              <Avatar v-else-if="item.icon === 'avatar'" class="text-4xl opacity-50" />
+              <MiniSweeper v-else-if="item.icon === 'minisweeper'" class="text-4xl opacity-50" />
+              <Looptap v-else-if="item.icon === 'looptap'" class="text-4xl opacity-50" />
+              <Pacman v-else-if="item.icon === 'pacman'" class="text-4xl opacity-50" />
+              <Tetris v-else-if="item.icon === 'tetris'" class="text-4xl opacity-50" />
+              <Decolorization v-else-if="item.icon === 'decolorization'" class="text-4xl opacity-50" />
+              <Vitetem v-else-if="item.icon === 'vitetem'" class="text-4xl opacity-50" />
+              <Mini v-else-if="item.icon === 'mini'" class="text-4xl opacity-50" />
+              <Life v-else-if="item.icon === 'life'" class="text-4xl opacity-50" />
+              <Car v-else-if="item.icon === 'car'" class="text-4xl opacity-50" />
+              <TetrisCli v-else-if="item.icon === 'tetrisCli'" class="text-4xl opacity-50" />
+              <History v-else-if="item.icon === 'history'" class="text-4xl opacity-50" />
+              <Unknown v-else class="text-4xl opacity-50" />
+            </div>
+            <div class="flex-auto">
+              <div cla ss="text-normal">{{ item.name }}</div>
+              <div class="desc text-sm opacity-50 font-normal" v-html="item.desc" />
+            </div>
+          </a>
         </div>
-        <div class="flex-auto">
-          <div cla ss="text-normal">{{ item.name }}</div>
-          <div class="desc text-sm opacity-50 font-normal" v-html="item.desc" />
-        </div>
-      </a>
+      </template>
     </div>
-  </template>
+
+    <div>
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   </div>
 </template>
@@ -58,6 +82,23 @@ import Venmo from './icon/Venmo.vue'
 import Highlight from './icon/Highlight.vue'
 import Typing from './icon/Typing.vue'
 import Unknown from './icon/Unknown.vue'
+import Jumper from './icon/Jumper.vue'
+import TZFE from './icon/TZFE.vue'
+import Sudoku from "./icon/Sudoku.vue"
+import Mathler from './icon/Mathler.vue'
+import Dwen from './icon/Dwen.vue'
+import Avatar from './icon/Avatar.vue'
+import MiniSweeper from './icon/Minisweeper.vue'
+import Looptap from './icon/Looptap.vue'
+import Pacman from './icon/Pacman.vue'
+import Tetris from './icon/Tetris.vue'
+import Decolorization from './icon/Decolorization.vue'
+import Vitetem from './icon/Vitetem.vue'
+import Mini from './icon/Mini.vue'
+import Life from './icon/Life.vue'
+import Car from './icon/Car.vue'
+import TetrisCli from './icon/TetrisCli.vue'
+import History from './icon/History.vue'
 </script>
 
 <style scoped>
