74ad675e0931c1f02b456601ccbaffa72e2ba7ff
xiaoxunyao <huchengyea@163.com>
2022-04-14 16:21:00 +0800

    chore: init

---
+++ .vitepress/theme/Projects.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="divide-y divide-gray-200">
+    
+    <div class="pt-6 pb-8 space-y-2 md:space-y-5">
+      <h1
+        class="
+          text-3xl
+          leading-9
+          font-extrabold
+          text-gray-900
+          tracking-tight
+          sm:text-4xl sm:leading-10
+          md:text-6xl md:leading-14
+        "
+      >
+        {{ $frontmatter.title }}
+      </h1>
+      <p class="text-lg leading-7 text-gray-500">
+        {{ $frontmatter.subtext }}
+      </p>
+    </div>
+
+    <div>
+      <template v-for="key in Object.keys($frontmatter.projects)" :key="key">
+    <h4 class="mt-10 font-bold">
+      {{ key }}
+    </h4>
+    <div class="project-grid py-2 -mx-3 gap-2">
+      <a
+        v-for="item, idx in $frontmatter.projects[key]"
+        :key="idx"
+        class="item relative flex items-center"
+        :href="item.link"
+        target="_blank"
+        :class="!item.link ? 'opacity-0 pointer-events-none h-0 -mt-8 -mb-4' : ''"
+      >
+        <div v-if="item.icon" class="pt-2 pr-5">
+          <Proskit v-if="item.icon === 'proskit'" class="text-4xl opacity-50" />
+          <Venmo v-if="item.icon === 'venmoAdmin'" class="text-4xl opacity-50" />
+          <div v-else class="text-3xl opacity-50" :class="item.icon || 'i-carbon-unknown'" />
+        </div>
+        <div class="flex-auto">
+          <div cla ss="text-normal">{{ item.name }}</div>
+          <div class="desc text-sm opacity-50 font-normal" v-html="item.desc" />
+        </div>
+      </a>
+    </div>
+  </template>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Proskit from './icon/Proskit.vue'
+import Venmo from './icon/Venmo.vue'
+</script>
+
+<style scoped>
+.project-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+}
+.project-grid a.item {
+  padding: 0.8em 1em;
+  background: transparent;
+  font-size: 1.1rem;
+}
+.project-grid a.item:hover {
+  background: #88888808;
+}
+</style>
---
+++ projects.md
@@ -0,0 +1,14 @@
+---
+title: Projects
+subtext: List of projects that I am proud of
+projects:
+  Upcoming:
+    - name: 'proskit'
+      link: 'https://github.com/elonehoo/proskit'
+      desc: 'a tool class that makes javascript very sweet.'
+      icon: 'proskit'
+    - name: 'venmo-admin'
+      link: 'https://github.com/elonehoo/venmo-admin'
+      desc: 'admin ui.'
+      icon: 'venmoAdmin'
+---
